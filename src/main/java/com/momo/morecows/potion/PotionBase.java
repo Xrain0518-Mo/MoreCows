57bfe5c7eeddae96af88edf2c1c0c73ecc330f1a
MoMo渣 <61381841+iMoMo-cn@users.noreply.github.com>
2021-08-28 11:13:55 +0800

    tipped arrows effect

--- src/main/java/com/momo/morecows/potion/PotionBase.java
+++ src/main/java/com/momo/morecows/potion/PotionBase.java
@@ -1,11 +1,14 @@
 package com.momo.morecows.potion;
 
 
+import com.momo.morecows.IdlFramework;
 import com.momo.morecows.util.EntityUtil;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nullable;
 import java.util.Collection;
@@ -35,7 +38,7 @@ public class PotionBase extends Potion {
         return true;
     }
 
-    //instantPotion
+
     public void affectEntity(@Nullable Entity source, @Nullable Entity indirectSource, EntityLivingBase entityLivingBaseIn, int amplifier, double health)
     {
         if(this == ModPotion.DELAYED)
@@ -57,11 +60,40 @@ public class PotionBase extends Potion {
     }
 
 
-    //continuousPotion
+    //tipped arrow effect
     public void performEffect(EntityLivingBase entityLivingBaseIn, int amplifier)
     {
+        if(this == ModPotion.DELAYED)
+        {
+            entityLivingBaseIn.removePotionEffect(this);
 
+            Collection<PotionEffect> activePotionEffects = entityLivingBaseIn.getActivePotionEffects();
+
+            for (int i = 0; i < activePotionEffects.size(); i++) {
+                PotionEffect effect = (PotionEffect)activePotionEffects.toArray()[i];
+                entityLivingBaseIn.addPotionEffect(new PotionEffect(effect.getPotion(), effect.getDuration() + (120 + amplifier*180)*20, effect.getAmplifier(), effect.getIsAmbient(), true));
+            }
+        }
+
+        else if(this == ModPotion.PURIFY)
+        {
+            for (int i = 0; i < 3 + amplifier*2; i++) EntityUtil.TryRemoveDebuff(entityLivingBaseIn);
+        }
     }
 
+    private static final ResourceLocation TEXTURE = new ResourceLocation(IdlFramework.MODID + ":textures/gui/potion.png");
 
+    @Override
+    public void renderInventoryEffect(int x, int y, PotionEffect e, Minecraft mc){
+        mc.getTextureManager().bindTexture(TEXTURE);
+        if(this == ModPotion.DELAYED) mc.currentScreen.drawTexturedModalRect(x + 6, y + 7, 0, 0, 18, 18);
+        else if(this == ModPotion.PURIFY) mc.currentScreen.drawTexturedModalRect(x + 6, y + 7, 18, 0, 18, 18);
+    }
+
+    @Override
+    public void renderHUDEffect(int x, int y, PotionEffect e, Minecraft mc, float alpha){
+        mc.getTextureManager().bindTexture(TEXTURE);
+        if(this == ModPotion.DELAYED) mc.ingameGUI.drawTexturedModalRect(x + 3, y + 3, 0, 0, 18, 18);
+        else if(this == ModPotion.PURIFY) mc.ingameGUI.drawTexturedModalRect(x + 3, y + 3, 18, 0, 18, 18);
+    }
 }
