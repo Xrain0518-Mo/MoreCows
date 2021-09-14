7137ed69ba1bdf6c46bdb7c4dbd6454952d0582e
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-09-14 14:57:20 +0800

    fix when slots not all empty don't exec.

--- src/main/java/com/momo/morecows/util/recipes/RecipesManager.java
+++ src/main/java/com/momo/morecows/util/recipes/RecipesManager.java
@@ -1,8 +1,8 @@
 package com.momo.morecows.util.recipes;
 
 import com.google.common.collect.Lists;
-import com.momo.morecows.util.ItemStackUtil;
 import com.momo.morecows.recipe.CheckRecipe;
+import com.momo.morecows.util.ItemStackUtil;
 import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
@@ -31,8 +31,7 @@ public class RecipesManager {
     public static void addMilkWorkshopRecipe(List<ItemStack> inputs, List<ItemStack> outputs, int progress) {
         if (inputs.size() != 2 || outputs.size() != 2) {
             throw new IllegalArgumentException("recipe input or output size must be equal to 2");
-        }
-        else {
+        } else {
             CheckRecipe recipeTemp = new CheckRecipe(inputs, outputs, progress);
             if (checkMilkWorkshopRecipeRepeat(recipeTemp)) {
                 milkWorkshopRecipes.add(recipeTemp);
@@ -45,16 +44,36 @@ public class RecipesManager {
         return milkWorkshopRecipes.stream().noneMatch(recipeTemp::equals);
     }
 
+    public static boolean containsRecipeWithEmpty(CheckRecipe recipeTemp) {
+        boolean flag = false;
+        for (CheckRecipe recipe : milkWorkshopRecipes) {
+            if (ItemStackUtil.areItemStackListEqual(recipe.getInputs(), recipeTemp.getInputs())) {
+                for (ItemStack output : recipeTemp.getOutputs()) {
+                    if (output.isEmpty()) {
+                        flag = true;
+                        continue;
+                    }
+                    for (ItemStack recipeOutput : recipe.getOutputs()) {
+                        if (ItemStackUtil.isItemStackEqual(recipeOutput, output)) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
     // don't output
     public static CheckRecipe getMilkWorkshopRecipe(CheckRecipe recipeTemp) {
         for (CheckRecipe recipe : milkWorkshopRecipes) {
             if (recipeTemp.getOutputs().get(0) != ItemStack.EMPTY || recipeTemp.getOutputs().get(1) != ItemStack.EMPTY) {
                 if (!checkMilkWorkshopRecipeRepeat(recipeTemp)) {
                     return recipe;
-                    // fix when current recipe has output, but input was another recipe
+                } else if (containsRecipeWithEmpty(recipeTemp)) {
+                    return recipe;
                 }
-            }
-            else if (ItemStackUtil.areItemStackListEqual(recipe.getInputs(), recipeTemp.getInputs())) {
+            } else if (ItemStackUtil.areItemStackListEqual(recipe.getInputs(), recipeTemp.getInputs())) {
                 return recipe;
             }
         }
